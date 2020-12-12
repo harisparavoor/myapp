7a2cb3dec7247dbe96b3c7e36a94453a5249b97e
harisparavoor <harisparavoor@gmail.com>
2020-12-12 17:10:42 +0530

    change name

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Hari.ps' });
+  res.render('index', { title: 'Hari.ps you' });
 });
 
 module.exports = router;
