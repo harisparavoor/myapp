746b482e2743fb7c7d704ab07617fd7d92ca15cf
harisparavoor <harisparavoor@gmail.com>
2020-12-30 00:03:43 +0530

    new file

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Hari.ps: you completed' });
+  res.render('index', { title: 'Hari.ps: you completed hai' });
 });
 
 module.exports = router;
