5ad9f5dd9865cd9259ba53bb382e12cd6bbc284b
harisparavoor <harisparavoor@gmail.com>
2021-01-19 18:42:28 +0530

    check

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Hari.ps: you hi ' });
+  res.render('index', { title: 'Hari.ps: you ' });
 });
 
 module.exports = router;
