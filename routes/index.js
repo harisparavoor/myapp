33c815a6adc27ec1317b911854a4160454d33f3c
harisparavoor <harisparavoor@gmail.com>
2021-04-03 23:45:58 +0530

    pod new svn

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Hari.ps:pods win scucces ' });
+  res.render('index', { title: 'Hari.ps: kude ' });
 });
 
 module.exports = router;
