a45e01d5f2c54fe436d518163837c03ef4b64675
harisparavoor <harisparavoor@gmail.com>
2021-04-18 20:02:53 +0530

    hha

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Hari.ps: kude coronna ' });
+  res.render('index', { title: 'Hari.ps: corona you ' });
 });
 
 module.exports = router;
