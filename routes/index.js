4395c1d332eeb73ea8ad6dfbdcfe10922ee45742
harisparavoor <harisparavoor@gmail.com>
2021-04-18 16:19:42 +0530

    text change

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Hari.ps: kude test succes ' });
+  res.render('index', { title: 'Hari.ps: kude test ' });
 });
 
 module.exports = router;
