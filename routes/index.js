cd7bde99b9dcd14a6837c85b0071aaf7b4d86600
harisparavoor <harisparavoor@gmail.com>
2021-09-26 16:54:52 +0530

    dep

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Hari.ps: corona end ' });
+  res.render('index', { title: 'Hari.ps: corona end 2022 ' });
 });
 
 module.exports = router;
