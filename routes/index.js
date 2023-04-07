6cc413928f1993f3f6ed5c07a981f7770b6915d6
Hari <hari@harips.work>
2023-04-07 13:51:40 +0100

    change year

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Hari.ps: corona end 2022 ' });
+  res.render('index', { title: 'Hari.ps: 2023 ' });
 });
 
 module.exports = router;
