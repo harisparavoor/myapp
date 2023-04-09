a7d1bc51e9ab9d117d5662c8e71eaed02ab12235
harisparavoor <harisparavoor@gmail.com>
2023-04-09 12:36:01 +0100

    image file changed

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Hari.ps: you 2023 ' });
+  res.render('index', { title: 'Hari.ps: you hi 2023 ' });
 });
 
 module.exports = router;
